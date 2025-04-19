54218173e94fdb21e9155347b9f360c8bbacb5fa
iyz <iy.usa@outlook.com>
2025-04-19 02:09:20 -0400

    add requestHandler function to modularize code
    - use try-with-resources to clean up open connections

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,92 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+  private static final String Protocol = "HTTP/1.1";
+  private static final String CRLF = "\r\n";
 
-  public static void main(String[] args) {
-    final String Protocol = "HTTP/1.1";
-    final String CRLF = "\r\n";
+  private static final String RespOK = "200 OK";
+  private static final String RespNotFound = "404 Not Found";
+
+  private static final String ContentTypeLength = "Content-Type: text/plain\r\nContent-Length: ";
   
-    final String RespOK = "200 OK";
-
+  public static void main(String[] args) {
     // Print statements for debugging - visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
     
     try (
       ServerSocket serverSocket = new ServerSocket(); // try-with-resources to automatically clean up ServerSocket
     ) {
-      serverSocket.setReuseAddress(true);
-      serverSocket.bind(new InetSocketAddress(4221));
-      Socket socket = serverSocket.accept(); // Wait for connection from client.
-      System.out.println("accepted new connection");
-
       // Since the tester restarts program quite often, setting SO_REUSEADDR
       // ensures that we don't run into 'Address already in use' errors
+      serverSocket.setReuseAddress(true);
+      serverSocket.bind(new InetSocketAddress(4221));
 
+      try (
+        Socket socket = serverSocket.accept(); // Wait for connection from client, gets automatically cleaned up at end
+      ) {
+        System.out.println("accepted new connection");
 
-      // DataInputStream dataInStream = new DataInputStream(socket.getInputStream());
-      // String stringData = dataInStream.readUTF();
-      // System.out.printf("**MESSAGE: %s", stringData);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream(), StandardCharsets.UTF_8));
 
-      // DataOutputStream dataOutStream = new DataOutputStream(socket.getOutputStream());
-      // dataOutStream.writeUTF(String.format("%s %s%s%s", Protocol, RespOK, CRLF, CRLF));
-      // dataOutStream.flush();
+        List<String> requestStrings = new ArrayList<>();
+        String tempString;
 
-      socket.getOutputStream().write((String.format("%s %s%s%s", Protocol, RespOK, CRLF, CRLF).getBytes(StandardCharsets.US_ASCII)));
-      socket.close();
+        while((tempString = bufferedReader.readLine()) != null && !tempString.isEmpty()) {
+          System.out.printf("Received: %s\n", tempString);
+          requestStrings.add(tempString);
+        }
+        handleRequest(requestStrings, socket);
+
+        socket.close();
+      }
+
     } catch (IOException e) {
       System.out.println("IOException: " + e.getMessage());
     }
 
   }
+
+  public static IOException handleRequest(List<String> requestParts, Socket socket) {
+    try {
+      String[] requestLineParts = requestParts.get(0).split(" ");
+      String[] pathStrings = requestLineParts[1].split("/");
+
+      if ("GET".equals(requestLineParts[0])) {
+        byte[] byteMessage;
+        if (pathStrings.length == 0) { // if original path was "/", respond 200 OK
+          byteMessage = String.format("%s %s%s%s", Protocol, RespOK, CRLF, CRLF).getBytes(StandardCharsets.US_ASCII);
+          socket.getOutputStream().write((byteMessage));
+        }
+        else if ("echo".equals(pathStrings[1])) { // send response where * after /echo/* is the body
+          byteMessage = String.format("%s %s%s%s%d%s%s%s", Protocol, RespOK, CRLF, ContentTypeLength, pathStrings[2].length(), CRLF, CRLF, pathStrings[2]).getBytes(StandardCharsets.US_ASCII);
+          socket.getOutputStream().write((byteMessage));
+        }
+        else if ("user-agent".equals(pathStrings[1])) { // send response where the User-Agent header's content is the body
+          String currentHeader;
+          for(int i = 1; i < requestParts.size(); i++) { // init "i" as 1 to skip to header section
+            currentHeader = requestParts.get(i);
+            if(currentHeader.contains("User-Agent:")) {
+              String[] userAgentParts = requestParts.get(i).split(" ");
+              byteMessage = String.format("%s %s%s%s%d%s%s%s", Protocol, RespOK, CRLF, ContentTypeLength, userAgentParts[1].length(), CRLF, CRLF, userAgentParts[1]).getBytes(StandardCharsets.US_ASCII);
+              socket.getOutputStream().write((byteMessage));
+            }
+          }
+        }
+      }
+    
+      socket.getOutputStream().write((String.format("%s %s%s%s", Protocol, RespNotFound, CRLF, CRLF).getBytes(StandardCharsets.US_ASCII))); // return 404 Not Found
+    } catch (IOException e) {
+      return e;
+    }
+    return null;
+  }
+
 }
